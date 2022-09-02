0aa7744e1dfd13e9e3493d71b369ff2816bb2fc3
sestrus <piotr.burdzy.klb@gmail.com>
2022-09-02 15:39:17 +0200

    adding input field

--- src/components/SearchWiki.js
+++ src/components/SearchWiki.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 
 const SearchWiki = () => {
+    const [term, setTerm] = useState('')
+
 
     return (
-        <div>
-            <h1>SearchWiki</h1>
+        <div className="ui container">
+            <div className="field">
+                <div className="field">
+                    <label>Search in Wikipedia</label>
+                    <input
+                        className="input"
+                        value={term}
+                        onChange={e => {
+                            setTerm(e.target.value);
+                            console.log(e.target.value)
+                        }}
+                    />
+                </div>
+            </div>
+
         </div>
     )
 }
