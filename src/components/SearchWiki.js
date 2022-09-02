f520917e95bc2c24eee9e24042fe33d4c70dab66
sestrus <piotr.burdzy.klb@gmail.com>
2022-09-02 15:47:50 +0200

    adding axios wikipedia api connection

--- src/components/SearchWiki.js
+++ src/components/SearchWiki.js
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
 
 
 
 
 const SearchWiki = () => {
     const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search)
+        };
+
+        if (term) { search() }
+
+    }, [term])
 
 
     return (
