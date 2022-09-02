6d1488c639f34f03f06670a96a8d55ad82a8b7f7
sestrus <piotr.burdzy.klb@gmail.com>
2022-09-02 15:58:36 +0200

    rendering items from api

--- src/components/SearchWiki.js
+++ src/components/SearchWiki.js
@@ -26,10 +26,31 @@ const SearchWiki = () => {
 
     }, [term])
 
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                        target="_blank"
+                    >
+                        Open Wiki
+                    </a>
+
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+
+        )
+    })
 
     return (
         <div className="ui container">
-            <div className="field">
+            <div className="ui form">
                 <div className="field">
                     <label>Search in Wikipedia</label>
                     <input
@@ -41,8 +62,9 @@ const SearchWiki = () => {
                         }}
                     />
                 </div>
-            </div>
 
+            </div>
+            <div className="ui celled list"> {renderedResults}</div>
         </div>
     )
 }
